249ae648d0e5fa48ec779de8f0d81763adfba4ba
GGG KILLER <gggkiller2@gmail.com>
2020-11-02 01:10:45 -0300

    Remove unnecessary stackalloc path.

--- MultiMc.SolderUpdater/VersionValueConverter.cs
+++ MultiMc.SolderUpdater/VersionValueConverter.cs
@@ -9,13 +9,7 @@ namespace MultiMc.SolderUpdater
         public override Version Read ( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options ) =>
             Version.Parse ( reader.GetString ( ) );
 
-        public override void Write ( Utf8JsonWriter writer, Version value, JsonSerializerOptions options )
-        {
-            Span<Char> buff = stackalloc Char[2 * 3 + 2];
-            if ( !value.TryFormat ( buff, 3, out var written ) )
-                writer.WriteStringValue ( buff.Slice ( 0, written ) );
-            else
-                writer.WriteStringValue ( value.ToString ( ) );
-        }
+        public override void Write ( Utf8JsonWriter writer, Version value, JsonSerializerOptions options ) =>
+            writer.WriteStringValue ( value.ToString ( 3 ) );
     }
 }
