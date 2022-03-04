8e891f6a7945aef070cba32556ad5693cf2c4846
GGG KILLER <gggkiller2@gmail.com>
2022-03-04 18:11:24 -0300

    Fix the solder API client.

--- MultiMc.SolderUpdater/Solder/SolderApiClient.cs
+++ MultiMc.SolderUpdater/Solder/SolderApiClient.cs
@@ -10,21 +10,21 @@ namespace MultiMc.SolderUpdater.Solder
     {
         public SolderApiClient ( Uri baseUri )
         {
-            this.BaseAddress = new Uri ( baseUri.GetLeftPart ( UriPartial.Authority ) );
+            this.BaseAddress = baseUri;
             this.DefaultRequestHeaders.UserAgent.Clear ( );
             this.DefaultRequestHeaders.UserAgent.Add ( new System.Net.Http.Headers.ProductInfoHeaderValue ( "MultiMc.SolderUpdater", Program.UpdaterVersion.ToString ( 3 ) ) );
             this.DefaultRequestVersion = new Version ( 2, 0 );
         }
 
         public async Task<ApiInfo> GetApiInfoAsync ( ) =>
-            await this.GetFromJsonAsync<ApiInfo> ( "/api" ).ConfigureAwait ( false );
+            await this.GetFromJsonAsync<ApiInfo> ( "api" ).ConfigureAwait ( false );
 
         public async Task<ModInfo> GetModInfoAsync ( String modName )
         {
             if ( String.IsNullOrWhiteSpace ( modName ) )
                 throw new ArgumentException ( "Mod name cannot be null, empty or composed of whitespaces.", nameof ( modName ) );
 
-            return await this.GetFromJsonAsync<ModInfo> ( $"/api/mod/{modName}" ).ConfigureAwait ( false );
+            return await this.GetFromJsonAsync<ModInfo> ( $"api/mod/{modName}" ).ConfigureAwait ( false );
         }
 
         public async Task<ModVersion> GetModVersionAsync ( String name, String version )
@@ -34,7 +34,7 @@ namespace MultiMc.SolderUpdater.Solder
             if ( String.IsNullOrWhiteSpace ( version ) )
                 throw new ArgumentException ( "Mod version cannot be null, empty or composed of whitespaces.", nameof ( version ) );
 
-            return await this.GetFromJsonAsync<ModVersion> ( $"/api/mod/{name}/{version}" ).ConfigureAwait ( false );
+            return await this.GetFromJsonAsync<ModVersion> ( $"api/mod/{name}/{version}" ).ConfigureAwait ( false );
         }
 
         public async Task<ModpackInfo> GetModpackInfoAsync ( String slug )
@@ -42,7 +42,7 @@ namespace MultiMc.SolderUpdater.Solder
             if ( String.IsNullOrWhiteSpace ( slug ) )
                 throw new ArgumentException ( "Modpack slug cannot be null, empty or composed of whitespaces.", nameof ( slug ) );
 
-            return await this.GetFromJsonAsync<ModpackInfo> ( $"/api/modpack/{slug}" ).ConfigureAwait ( false );
+            return await this.GetFromJsonAsync<ModpackInfo> ( $"api/modpack/{slug}" ).ConfigureAwait ( false );
         }
 
         public async Task<ModpackBuild> GetModpackBuildAsync ( String slug, String build )
@@ -52,7 +52,7 @@ namespace MultiMc.SolderUpdater.Solder
             if ( String.IsNullOrWhiteSpace ( build ) )
                 throw new ArgumentException ( "Modpack build cannot be null, empty or composed of whitespaces.", nameof ( build ) );
 
-            return await this.GetFromJsonAsync<ModpackBuild> ( $"/api/modpack/{slug}/{build}" );
+            return await this.GetFromJsonAsync<ModpackBuild> ( $"api/modpack/{slug}/{build}" );
         }
     }
 }
